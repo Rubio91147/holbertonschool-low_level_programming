596b2211147bf4eef1469201f4a8e42b84561022
rubio91147 <rubendariofb@gmail.com>
2023-02-17 07:47:35 -0800

    4. Hello, puts

--- hello_world/4-puts.c
+++ hello_world/4-puts.c
@@ -1,9 +1,12 @@
-#!/bin/bash
 #include <stdio.h>
+
 /**
-*main - a C program that prints exactly "Programming is like building
-*Return: 0 (Success)
-*/
+ * main - function
+ * description - a C program that prints exactly "Programming is like building
+ *
+ * Return: 0 (Success)
+ *
+ */
 int main(void)
 {
 puts("\"Programming is like building a multilingual puzzle");

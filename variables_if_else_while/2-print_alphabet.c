008ffe260b86f27055b89f6fa184d4e12726cb30
rubio91147 <rubendariofb@gmail.com>
2023-02-20 15:55:08 -0800

    putchar

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -1,6 +1,11 @@
 #include <stdlib.h>
 #include <stdio.h>
 #include <time.h>
+/**
+*main - Entry point
+*
+*Return: Always 0 (success)
+*/
 int main(void)
 {
 char alfabeto[24] = {'a', 'b', 'c', 'd', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'y', 'z'};

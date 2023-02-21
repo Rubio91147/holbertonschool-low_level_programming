5a0e64826221c19e1779969925a1edd5084cf2e8
rubio91147 <rubendariofb@gmail.com>
2023-02-20 16:14:21 -0800

    putchar

--- variables_if_else_while/2-print_alphabet.c
+++ variables_if_else_while/2-print_alphabet.c
@@ -8,10 +8,7 @@
 */
 int main(void)
 {
-char alfabeto[24] = {'a', 'b', 'c', 'd', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'x', 'y', 'z'};
-int i = 0;
-for (i = 0; i <= 24; i++)
-{
-putchar(alfabeto[i]);
-}
+char alfabeto = 'a';
+for (i = 'a'; i <= 'z'; i++)
+putchar(alfabeto);
 }

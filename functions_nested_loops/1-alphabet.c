95de62b3ba4a11bd3e44e58f642fb5f7a6caa5b8
rubio91147 <rubendariofb@gmail.com>
2023-02-21 11:34:24 -0800

    Write a function that prints the alphabet, in lowercase, followed by a new line.

---
+++ functions_nested_loops/1-alphabet.c
@@ -0,0 +1,24 @@
+#include "main.h"
+#include <stdlib.h>
+#include <stdio.h>
+#include <time.h>
+#include <ctype.h>
+/* more headers goes there */
+/* betty style doc for function main goes there */
+/**
+*main - Entry point
+*
+*Return: Always 0 (success)
+*/
+void print_alphabet(void);
+int main (void)
+{
+char alfabeto = 'a';
+for (alfabeto = 'a'; alfabeto <= 'z'; alfabeto++)
+{
+putchar(alfabeto);
+putchar(\n);
+}
+}
+return(o);
+
--- functions_nested_loops/README.md
+++ functions_nested_loops/README.md
@@ -1,3 +1,4 @@
 0-Write a program that prints _putchar, followed by a new line.
 
 The program should return 0
+1. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game

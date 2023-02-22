8b0d01a4967e56ef7ab7f4245e5a3481f01f2c66
rubio91147 <rubendariofb@gmail.com>
2023-02-22 10:15:40 -0800

    5. Sign

---
+++ functions_nested_loops/5-sign.c
@@ -0,0 +1,18 @@
+#include "main.h"
+
+/**
+* print_sign - function
+*@n: character in upper or lower case
+*description: chcks if n numbers  lovercase character
+*
+*Returns 1 o 0 o -1
+*/
+int print_sign(int n)
+{
+if (n > 0)
+return (1);
+else if (n == 0)
+return (0);
+else
+return (-1);
+}

7aeb95a21fc8a776c19567a1fbf7e0e2456d13fa
rubio91147 <rubendariofb@gmail.com>
2023-02-23 07:11:20 -0800

    10. a + b

---
+++ functions_nested_loops/10-add.c
@@ -0,0 +1,14 @@
+#include "main.h"
+/**
+* add - adds two integers and returns the result.
+* description = add  of current result
+* @x: is the argument in the funtion
+* @y: is the argument in the funtion
+* Return: result add un num
+*/
+int add(int x, int y)
+{
+int result;
+result = x + y;
+return (result);
+}
--- functions_nested_loops/README.md
+++ functions_nested_loops/README.md
@@ -3,3 +3,4 @@
 The program should return 0
 1. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game
 2.2. 10 x alphabet
+10: Write a function that adds two integers and returns the result.

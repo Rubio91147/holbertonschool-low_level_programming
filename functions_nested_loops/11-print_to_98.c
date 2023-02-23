b274d2a420b34d1c65e930bb6adecd7d9119d4c7
rubio91147 <rubendariofb@gmail.com>
2023-02-23 08:15:16 -0800

    11. 98 Battery Street, the OG

--- functions_nested_loops/11-print_to_98.c
+++ functions_nested_loops/11-print_to_98.c
@@ -1,25 +1,23 @@
-#include "main.h"
 #include <stdio.h>
-/** 
- * print_to_98 - funtion
- * description - print all numbers  natural 
- * @n: argument for at funtion
- *
- * Return: 0
-*/
+
+/**
+ * print_to_98 - Prints all natural numbers from input to 98,
+ * in order separated by a comma followed by a space.
+ * @n: The number to begin counting at.
+ */
 
 void print_to_98(int n)
 {
 if (n >= 98)
 {
 while (n > 98)
-printf("%d, " n--);
-printf("%d", n);
+printf("%d, ", n--);
+printf("%d\n", n);
 }
-else 
+else
 {
 while (n < 98)
-printf("%d, " n++);
-printf("%d", n);
+printf("%d, ", n++);
+printf("%d\n", n);
 }
 }

d7b50dad63efdeb3180b8a2ba32c24b6bdc0f52c
Rubio91147 <124604809+Rubio91147@users.noreply.github.com>
2023-02-22 20:10:04 -0500

    Update 9-times_table.c

--- functions_nested_loops/9-times_table.c
+++ functions_nested_loops/9-times_table.c
@@ -7,17 +7,16 @@
 */
 void times_table(void)
 {
-int i, j, incremento, tab;
-for (i = 0; i <= 9; i = incremento)
-{
-for (j = 0; j <= 9; j++)
-{
-tab = i * j;
-printf("%02d", tab);
-if (j != 9)
-printf(", ");
-}
-printf("\n");
-incremento++;
-}
+    int i,j, incremento, tab;
+    for(i = 0; i <= 9; i = incremento){
+        
+        for(j= 0; j <= 9; j++){
+            tab = i * j;
+            printf("%2d",tab);
+            if(j != 9)
+            printf(", ");
+        }
+        printf("\n");
+        incremento++;
+    }
 }

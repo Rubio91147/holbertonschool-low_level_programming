ac10ff17153831c07570fb1356a6fd1cb4e7abf4
rubio91147 <rubendariofb@gmail.com>
2023-02-23 07:56:50 -0800

    11. 98 Battery Street, the OG

---
+++ functions_nested_loops/11-print_to_98.c
@@ -0,0 +1,25 @@
+#include "main.h"
+#include <stdio.h>
+/** 
+ * print_to_98 - funtion
+ * description - print all numbers  natural 
+ * @n: argument for at funtion
+ *
+ * Return: 0
+*/
+
+void print_to_98(int n)
+{
+if (n >= 98)
+{
+while (n > 98)
+printf("%d", n--);
+printf("%d", n);
+}
+else 
+{
+while (n < 98)
+printf("%d", n++);
+printf("%d", n);
+}
+}
--- functions_nested_loops/README.md
+++ functions_nested_loops/README.md
@@ -4,3 +4,4 @@ The program should return 0
 1. I sometimes suffer from insomnia. And when I can't fall asleep, I play what I call the alphabet game
 2.2. 10 x alphabet
 10: Write a function that adds two integers and returns the result.
+11: Write a function that prints all natural numbers from n to 98, followed by a new line

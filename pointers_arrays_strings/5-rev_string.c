e6bc6ba00abdcf081a45a98620458d35cc390a17
Holberton <holberton@Holbertons-iMac.local>
2023-02-28 13:27:33 -0500

    Upload task 5  version 2

--- pointers_arrays_strings/5-rev_string.c
+++ pointers_arrays_strings/5-rev_string.c
@@ -7,15 +7,16 @@
  */
 void rev_string(char *s)
 {
-int k = 0;
-int len = 0;
-char aux;
-while (s[k++])
-len++;
-for (k = len - 1; k >= len / 2; k--)
-{
-aux = s[k];
-s[k] = s[len - k - 1];
-s[len - k - 1] = aux;
-}
+		int k = 0, len = 0;
+
+		char aux;
+		while (s[k++])
+				len++;
+
+		for (k = len - 1; k >= len / 2; k--)
+		{
+				aux = s[k];
+				s[k] = s[len - k - 1];
+				s[len - k - 1] = aux;
+		}
 }

1900eb1974452dee205dd9b64a0b73165b511cb6
Holberton <holberton@Holbertons-iMac.local>
2023-02-28 12:55:55 -0500

    write a functionthat reverce a string

--- pointers_arrays_strings/4-print_rev.c
+++ pointers_arrays_strings/4-print_rev.c
@@ -6,11 +6,12 @@
  */
 void print_rev(char *s)
 {
-int c;
-for (c = 0; s[c] != 0; c++)
-{
-}
-for (c = c - 1; c >= 0; c--)
-_putchar(s[c]);
-_putchar('\n');
-}
+  int c;
+
+  for (c = 0; s[c] != 0; c++)
+  {
+  }ccxx
+  for (c = c - 1; c >= 0; c--)
+    _putchar(s[c]);
+  _putchar('\n')
+			}

49dd495b4dad02c16f27ff1f1e509cd7e1740706
rubio91147 <rubendariofb@gmail.com>
2023-03-14 16:29:40 -0700

    Write a function that returns a pointer to a 2 dimensional array of integers.

--- malloc_free/2-str_concat.c
+++ malloc_free/2-str_concat.c
@@ -33,6 +33,26 @@ if (concat == NULL)
 return (NULL);
 len = 0;
 }
+char *concat;
+unsigned int len = n, index;
+if (s1 == NULL)
+{
+s1 = "";
+}
+if (s2 == NULL)
+{
+s2 = "";
+}
+for (index = 0; s1[index]; index++)
+{
+len++;
+concat = malloc(sizeof(char) * (len + 1));
+}
+if (concat == NULL)
+{
+return (NULL);
+len = 0;
+}
 for (index = 0; s1[index]; index++)
 {
 concat[len++] = s1[index];

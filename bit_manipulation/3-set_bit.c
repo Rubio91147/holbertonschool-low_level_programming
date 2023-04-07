e108c43470f7bc4949eac3957fc610cfb44d8b96
rubio91147 <rubendariofb@gmail.com>
2023-04-07 16:00:41 -0700

    Write a function that sets the value of a bit to 1 at a given index.

---
+++ bit_manipulation/3-set_bit.c
@@ -0,0 +1,19 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * set_bit - sets a bit at a given location
+ * @n: pointer to an unsigned long int
+ * @index: index of the bit you want to set
+ * Return: (Success)
+ */
+
+int set_bit(unsigned long int *n, unsigned int index)
+{
+int bit;
+if (index >> 31)
+return (-1);
+bit = 1 << index;
+*n = *n | bit;
+return (1);
+}

0a94fdec9bfcadfdd44ece772621d8e5c4845af2
rubio91147 <rubendariofb@gmail.com>
2023-04-11 18:21:05 -0700

    Write a hash function implementing the djb2 algorithm.

---
+++ hash_tables/1-djb2.c
@@ -0,0 +1,19 @@
+#include "hash_tables.h"
+
+/**
+ * hash_djb2 - hashing algorithm created by dan bernstein
+ * @str: string to hash together into an int
+ * Return: Returns a hashed representation of the given string
+ */
+
+unsigned long int hash_djb2(const unsigned char *str)
+{
+unsigned long int hash;
+int c;
+hash = 5381;
+while ((c = *str++))
+{
+hash = ((hash << 5) + hash) +c;
+}
+return (hash);
+}
